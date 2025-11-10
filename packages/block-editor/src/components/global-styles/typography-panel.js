642db10013c37f8c492e0ff25bb3762f1da34b2c
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2025-11-10 19:18:33 +1000

    Make text indent show by default as expected in global styles panel

--- packages/block-editor/src/components/global-styles/typography-panel.js
+++ packages/block-editor/src/components/global-styles/typography-panel.js
@@ -175,7 +175,7 @@ const DEFAULT_CONTROLS = {
 	textAlign: true,
 	textTransform: true,
 	textDecoration: true,
-	textIndent: false,
+	textIndent: true,
 	writingMode: true,
 	textColumns: true,
 };
