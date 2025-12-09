723d51acc0bb1557b05207a1d157ff6a8b38ee16
ingeniumed <ingeniumed@users.noreply.github.com>
2025-12-09 14:27:50 +1100

    Add reactive enforced mode for code editor

--- packages/core-data/src/utils/crdt.ts
+++ packages/core-data/src/utils/crdt.ts
@@ -86,6 +86,8 @@ const allowedPostProperties = new Set< string >( [
 	'tags',
 	'template',
 	'title',
+	'enforcedMode',
+	'enforcedModeOwner',
 ] );
 
 // Post meta keys that should *not* be synced.
